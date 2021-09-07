8e95843b93c522e2b8bb3220ee2436417a526305
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-09-06 19:25:14 -0500

    pt1 done

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Events.Add("Strange Loop");
+            Events.Add("Grace Hopper");
+            Events.Add("Code With Pride");
+            ViewBag.events = Events;
+            return View();
+        }
+    }
+}
